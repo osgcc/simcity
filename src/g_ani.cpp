0f241ded44e093a9840680c354525d383ba65f6b
Leeor Dicker <leeor_net@yahoo.com>
2022-02-01 23:59:28 -0500

    Grooming

--- src/g_ani.cpp
+++ src/g_ani.cpp
@@ -64,35 +64,28 @@
 
 
 /* comefrom: moveWorld doEditWindow scoreDoer doMapInFront graphDoer */
-animateTiles(void)
+void animateTiles()
 {
-  unsigned short tilevalue, tileflags;
-  unsigned short *tMapPtr;
-  int i;
+    unsigned short tilevalue, tileflags;
+    unsigned short* tMapPtr;
+    int i;
 
-  /* Animate whole world */
-  tMapPtr = (unsigned short *)&(Map[0][0]);
+    /* Animate whole world */
+    tMapPtr = (unsigned short*)&(Map[0][0]);
 
-  for (i = WORLD_X * WORLD_Y; i > 0; i--) {
-    tilevalue = (*tMapPtr);
-    if (tilevalue & ANIMBIT) {
-      tileflags = tilevalue & ALLBITS;
-      tilevalue &= LOMASK;
-#if 0
-      do {
-	tilevalue = aniTile[tilevalue];
-      } while (!(tileSynch & aniSynch[tilevalue]));
-#else
-      tilevalue = aniTile[tilevalue];
-#endif
+    for (i = WORLD_X * WORLD_Y; i > 0; i--)
+    {
+        tilevalue = (*tMapPtr);
+        if (tilevalue & ANIMBIT)
+        {
+            tileflags = tilevalue & ALLBITS;
+            tilevalue &= LOMASK;
 
-      tilevalue |= tileflags;
-      (*tMapPtr) = tilevalue;
+            tilevalue = aniTile[tilevalue];
+
+            tilevalue |= tileflags;
+            (*tMapPtr) = tilevalue;
+        }
+        tMapPtr++;
     }
-    tMapPtr++;
-  }
-
-#if 0
-  tileSynch = (tileSynch == 0x80) ? 0x01 : (tileSynch << 1);
-#endif
 }
