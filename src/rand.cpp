1566bb1879de1b0b872f7a04ef8112df50f38230
Leeor Dicker <leeor_net@yahoo.com>
2022-02-02 21:57:26 -0500

    Clean source

--- src/rand.cpp
+++ src/rand.cpp
@@ -33,23 +33,20 @@
 
 #include "main.h"
 
-#include <sys/types.h>
 #include <stdlib.h>
 
-#define SIM_RAND_MAX 0xffff
+constexpr auto SIM_RAND_MAX = 0xffff;
 
 static unsigned long next = 1;
 
-int
-sim_rand()
+int sim_rand()
 {
 	next = next * 1103515245 + 12345;
-	return ((next % ((SIM_RAND_MAX + 1) <<8)) >>8);
+	return (next % ((SIM_RAND_MAX + 1) << 8)) >> 8;
 }
 
-void
-sim_srand(seed)
-u_int seed;
+
+void sim_srand(unsigned int seed)
 {
 	next = seed;
 }
