5a06c46b910a42f5586e750960b71cdc02e71eea
Leeor Dicker <leeor_net@yahoo.com>
2022-02-04 00:04:05 -0500

    Avoid prototypes

--- src/s_scan.cpp
+++ src/s_scan.cpp
@@ -76,6 +76,27 @@ int CrimeMaxX, CrimeMaxY;
 int DonDither = 0;
 
 
+/* comefrom: FireAnalysis */
+void SmoothFSMap()
+{
+  register int x, y, edge;
+
+  for (x = 0; x < SmX; x++)
+    for (y = 0; y < SmY; y++) {
+      edge = 0;
+      if (x) edge += FireStMap[x - 1][y];
+      if (x < (SmX - 1)) edge += FireStMap[x + 1][y];
+      if (y) edge += FireStMap[x][y - 1];
+      if (y < (SmY - 1)) edge += FireStMap[x][y + 1];
+      edge = (edge >>2) + FireStMap[x][y];
+      STem[x][y] = edge >>1;
+    }
+  for (x = 0; x < SmX; x++)
+    for (y = 0; y < SmY; y++)
+      FireStMap[x][y] = STem[x][y];
+}
+
+
 /* comefrom: Simulate SpecialInit */
 void FireAnalysis(void)		/* Make firerate map from firestation map  */
 {
@@ -92,52 +113,15 @@ void FireAnalysis(void)		/* Make firerate map from firestation map  */
 }
 
 
-/* comefrom: Simulate SpecialInit */
-void PopDenScan(void)		/*  sets: PopDensity, , , ComRate  */
+/* comefrom: PopDenScan */
+void ClrTemArray()
 {
-  int Xtot, Ytot, Ztot;
   register int x, y, z;
 
-  ClrTemArray();
-  Xtot = 0;
-  Ytot = 0;
-  Ztot = 0;
-  for (x = 0; x < WORLD_X; x++)
-    for (y = 0; y < WORLD_Y; y++) {
-      z = Map[x][y];
-      if (z & ZONEBIT) {
-	z = z & LOMASK;
-	SMapX = x;
-	SMapY = y;
-	z = GetPDen(z) <<3;
-	if (z > 254)
-	  z = 254;
-	tem[x >>1][y >>1] = z;
-	Xtot += x;
-	Ytot += y;
-	Ztot++;
-      }
-    }
-  DoSmooth();			/* T1 -> T2 */
-  DoSmooth2();			/* T2 -> T1 */
-  DoSmooth();			/* T1 -> T2 */
-
+  z = 0;
   for (x = 0; x < HWLDX; x++)
     for (y = 0; y < HWLDY; y++)
-      PopDensity[x][y] = tem2[x][y] <<1;
-
-  DistIntMarket();		/* set ComRate w/ (/ComMap) */
-
-  if (Ztot) {			/* Find Center of Mass for City */
-    CCx = Xtot / Ztot;
-    CCy = Ytot / Ztot;
-  } else {
-    CCx = HWLDX;		/* if pop=0 center of Map is CC */
-    CCy = HWLDY;
-  }
-  CCx2 = CCx >>1;
-  CCy2 = CCy >>1;
-  NewMapFlags[DYMAP] = NewMapFlags[PDMAP] = NewMapFlags[RGMAP] = 1;
+      tem[x][y] = z;
 }
 
 
@@ -165,220 +149,6 @@ int GetPDen(int Ch9)
   return (0);
 }
 
-
-/* comefrom: Simulate SpecialInit */
-void PTLScan()   	/* Does pollution, terrain, land value   */
-{
-  int ptot, LVtot;
-  register int x, y, z, dis;
-  int Plevel, LVflag, loc, zx, zy, Mx, My, pnum, LVnum, pmax;
-
-  for (x = 0; x < QWX; x++)
-    for (y = 0; y < QWY; y++)
-      Qtem[x][y] = 0;
-  LVtot = 0;
-  LVnum = 0;
-  for (x = 0; x < HWLDX; x++)
-    for (y = 0; y < HWLDY; y++) {
-      Plevel = 0;
-      LVflag = 0;
-      zx = x <<1;
-      zy = y <<1;
-      for (Mx = zx; Mx <= zx + 1; Mx++)
-	for (My = zy; My <= zy + 1; My++) {
-	  if (loc = (Map[Mx][My] & LOMASK)) {
-	    if (loc < RUBBLE) {
-	      Qtem[x >>1][y >>1] += 15;	/* inc terrainMem */
-	      continue;
-	    }
-	    Plevel += GetPValue(loc);
-	    if (loc >= ROADBASE)
-	      LVflag++;
-	  }
-	}
-/* XXX ???
-      if (Plevel < 0)
-	Plevel = 250;
-*/
-      if (Plevel > 255)
-	Plevel = 255;
-      tem[x][y] = Plevel;
-      if (LVflag) {			/* LandValue Equation */
-	dis = 34 - GetDisCC(x, y);
-	dis = dis <<2;
-	dis += (TerrainMem[x >>1][y >>1] );
-	dis -= (PollutionMem[x][y]);
-	if (CrimeMem[x][y] > 190) dis -= 20;
-	if (dis > 250) dis = 250;
-	if (dis < 1) dis = 1;
-	LandValueMem[x][y] = dis;
-	LVtot += dis;
-	LVnum++;
-      } else
-	LandValueMem[x][y] = 0;
-    }
-
-  if (LVnum)
-    LVAverage = LVtot / LVnum;
-  else
-    LVAverage = 0;
-
-  DoSmooth();
-  DoSmooth2();
-  pmax = 0;
-  pnum = 0;
-  ptot = 0;
-  for (x = 0; x < HWLDX; x++) {
-    for (y = 0; y < HWLDY; y++)  {
-      z = tem[x][y];
-      PollutionMem[x][y] = z;
-      if (z) {				/*  get pollute average  */
-	pnum++;
-	ptot += z;
-	/* find max pol for monster  */
-	if ((z > pmax) ||
-	    ((z == pmax) && (!(Rand16() & 3)))) {
-	  pmax = z;
-	  PolMaxX = x <<1;
-	  PolMaxY = y <<1;
-	}
-      }
-    }
-  }
-  if (pnum)
-    PolluteAverage = ptot / pnum;
-  else
-    PolluteAverage = 0;
-
-  SmoothTerrain();
-
-  NewMapFlags[DYMAP] = NewMapFlags[PLMAP] = NewMapFlags[LVMAP] = 1;
-}
-
-
-/* comefrom: PTLScan */
-int GetPValue(int loc)
-{
-  if (loc < POWERBASE) {
-    if (loc >= HTRFBASE) return (/* 25 */ 75);	/* heavy traf  */
-    if (loc >= LTRFBASE) return (/* 10 */ 50);	/* light traf  */
-    if (loc <  ROADBASE) {
-      if (loc > FIREBASE) return (/* 60 */ 90);
-      /* XXX: Why negative pollution from radiation? */
-      if (loc >= RADTILE) return (/* -40 */ 255);	/* radioactivity  */
-    }
-    return (0);
-  }
-  if (loc <= LASTIND) return (0);
-  if (loc < PORTBASE) return (50);	/* Ind  */
-  if (loc <= LASTPOWERPLANT) return (/* 60 */ 100);	/* prt, aprt, cpp */
-  return (0);
-}
-
-
-/* comefrom: PTLScan DistIntMarket */
-int GetDisCC(int x, int y)
-{
-  int xdis, ydis, z;
-
-  if (x > CCx2)
-    xdis = x - CCx2;
-  else
-    xdis = CCx2 - x;
-
-  if (y > CCy2)
-    ydis = y - CCy2;
-  else
-    ydis = CCy2 - y;
-
-  z = (xdis + ydis);
-  if (z > 32)
-    return (32);
-  else
-    return (z);
-}
-
-
-/* comefrom: Simulate SpecialInit */
-void CrimeScan()
-{
-  int numz;
-  int totz;
-  register int x, y, z;
-  int cmax;
-
-  SmoothPSMap();
-  SmoothPSMap();
-  SmoothPSMap();
-  totz = 0;
-  numz = 0;
-  cmax = 0;
-  for (x = 0; x < HWLDX; x++)
-    for (y = 0; y < HWLDY; y++) {
-      if (z = LandValueMem[x][y]) {
-	++numz;
-	z = 128 - z;
-	z += PopDensity[x][y];
-	if (z > 300) z = 300;
-	z -= PoliceMap[x >>2][y >>2];
-	if (z > 250) z = 250;
-	if (z < 0) z = 0;
-	CrimeMem[x][y] = z;
-	totz += z;
-	if ((z > cmax) ||
-	    ((z == cmax) && (!(Rand16() & 3)))) {
-	  cmax = z;
-	  CrimeMaxX = x <<1;
-	  CrimeMaxY = y <<1;
-	}
-      } else {
-	CrimeMem[x][y] = 0;
-      }
-    }
-  if (numz)
-    CrimeAverage = totz / numz;
-  else
-    CrimeAverage = 0;
-  for (x = 0; x < SmX; x++)
-    for (y = 0; y < SmY; y++)
-      PoliceMapEffect[x][y] = PoliceMap[x][y];
-  NewMapFlags[DYMAP] = NewMapFlags[CRMAP] = NewMapFlags[POMAP] = 1;
-}
-
-
-/* comefrom: PTLScan */
-void SmoothTerrain()
-{
-  if (DonDither & 1) {
-    register int x, y = 0, z = 0, dir = 1;
-
-    for (x = 0; x < QWX; x++) {
-      for (; y != QWY && y != -1; y += dir) {
-	z += Qtem[(x == 0) ? x : (x - 1)][y] +
-	     Qtem[(x == (QWX - 1)) ? x : (x + 1)][y] +
-	     Qtem[x][(y == 0) ? (0) : (y - 1)] +
-	     Qtem[x][(y == (QWY - 1)) ? y : (y + 1)] +
-	     (Qtem[x][y] <<2);
-	TerrainMem[x][y] = (unsigned char)(((unsigned)z) >>3);
-	z &= 0x7;
-      }
-      dir = -dir;
-      y += dir;
-    }
-  } else {
-    register int x,y,z;
-
-    for (x = 0; x < QWX; x++)
-      for (y = 0; y < QWY; y++) {
-	z = 0;
-	if (x > 0) z += Qtem[x - 1][y];
-	if (x < (QWX - 1)) z += Qtem[x + 1][y];
-	if (y > 0) z += Qtem[x][y - 1];
-	if (y < (QWY - 1)) z += Qtem[x][y + 1];
-	TerrainMem[x][y] = (unsigned char)((z >>2) + Qtem[x][y]) >>1;
-      }
-  }
-}
 
 /* comefrom: PopDenScan */
 void DoSmooth ()        /* smooths data in tem[x][y] into tem2[x][y]  */
@@ -474,36 +244,145 @@ void DoSmooth2 ()        /* smooths data in tem2[x][y] into tem[x][y]  */
 }
 
 
-/* comefrom: PopDenScan */
-void ClrTemArray()
+/* comefrom: PTLScan */
+int GetPValue(int loc)
 {
-  register int x, y, z;
-
-  z = 0;
-  for (x = 0; x < HWLDX; x++)
-    for (y = 0; y < HWLDY; y++)
-      tem[x][y] = z;
+  if (loc < POWERBASE) {
+    if (loc >= HTRFBASE) return (/* 25 */ 75);	/* heavy traf  */
+    if (loc >= LTRFBASE) return (/* 10 */ 50);	/* light traf  */
+    if (loc <  ROADBASE) {
+      if (loc > FIREBASE) return (/* 60 */ 90);
+      /* XXX: Why negative pollution from radiation? */
+      if (loc >= RADTILE) return (/* -40 */ 255);	/* radioactivity  */
+    }
+    return (0);
+  }
+  if (loc <= LASTIND) return (0);
+  if (loc < PORTBASE) return (50);	/* Ind  */
+  if (loc <= LASTPOWERPLANT) return (/* 60 */ 100);	/* prt, aprt, cpp */
+  return (0);
 }
 
 
-/* comefrom: FireAnalysis */
-void SmoothFSMap()
+/* comefrom: PTLScan DistIntMarket */
+int GetDisCC(int x, int y)
 {
-  register int x, y, edge;
+  int xdis, ydis, z;
+
+  if (x > CCx2)
+    xdis = x - CCx2;
+  else
+    xdis = CCx2 - x;
+
+  if (y > CCy2)
+    ydis = y - CCy2;
+  else
+    ydis = CCy2 - y;
+
+  z = (xdis + ydis);
+  if (z > 32)
+    return (32);
+  else
+    return (z);
+}
+
+
+/* comefrom: PopDenScan */
+void DistIntMarket()
+{
+  register int x, y, z;
 
   for (x = 0; x < SmX; x++)
     for (y = 0; y < SmY; y++) {
-      edge = 0;
-      if (x) edge += FireStMap[x - 1][y];
-      if (x < (SmX - 1)) edge += FireStMap[x + 1][y];
-      if (y) edge += FireStMap[x][y - 1];
-      if (y < (SmY - 1)) edge += FireStMap[x][y + 1];
-      edge = (edge >>2) + FireStMap[x][y];
-      STem[x][y] = edge >>1;
+      z = GetDisCC(x <<2,y <<2);
+      z = z <<2;
+      z = 64 - z;
+      ComRate[x][y] = z;
+  }
+}
+
+
+/* comefrom: Simulate SpecialInit */
+void PopDenScan(void)		/*  sets: PopDensity, , , ComRate  */
+{
+  int Xtot, Ytot, Ztot;
+  register int x, y, z;
+
+  ClrTemArray();
+  Xtot = 0;
+  Ytot = 0;
+  Ztot = 0;
+  for (x = 0; x < WORLD_X; x++)
+    for (y = 0; y < WORLD_Y; y++) {
+      z = Map[x][y];
+      if (z & ZONEBIT) {
+	z = z & LOMASK;
+	SMapX = x;
+	SMapY = y;
+	z = GetPDen(z) <<3;
+	if (z > 254)
+	  z = 254;
+	tem[x >>1][y >>1] = z;
+	Xtot += x;
+	Ytot += y;
+	Ztot++;
+      }
     }
-  for (x = 0; x < SmX; x++)
-    for (y = 0; y < SmY; y++)
-      FireStMap[x][y] = STem[x][y];
+  DoSmooth();			/* T1 -> T2 */
+  DoSmooth2();			/* T2 -> T1 */
+  DoSmooth();			/* T1 -> T2 */
+
+  for (x = 0; x < HWLDX; x++)
+    for (y = 0; y < HWLDY; y++)
+      PopDensity[x][y] = tem2[x][y] <<1;
+
+  DistIntMarket();		/* set ComRate w/ (/ComMap) */
+
+  if (Ztot) {			/* Find Center of Mass for City */
+    CCx = Xtot / Ztot;
+    CCy = Ytot / Ztot;
+  } else {
+    CCx = HWLDX;		/* if pop=0 center of Map is CC */
+    CCy = HWLDY;
+  }
+  CCx2 = CCx >>1;
+  CCy2 = CCy >>1;
+  NewMapFlags[DYMAP] = NewMapFlags[PDMAP] = NewMapFlags[RGMAP] = 1;
+}
+
+
+/* comefrom: PTLScan */
+void SmoothTerrain()
+{
+  if (DonDither & 1) {
+    register int x, y = 0, z = 0, dir = 1;
+
+    for (x = 0; x < QWX; x++) {
+      for (; y != QWY && y != -1; y += dir) {
+	z += Qtem[(x == 0) ? x : (x - 1)][y] +
+	     Qtem[(x == (QWX - 1)) ? x : (x + 1)][y] +
+	     Qtem[x][(y == 0) ? (0) : (y - 1)] +
+	     Qtem[x][(y == (QWY - 1)) ? y : (y + 1)] +
+	     (Qtem[x][y] <<2);
+	TerrainMem[x][y] = (unsigned char)(((unsigned)z) >>3);
+	z &= 0x7;
+      }
+      dir = -dir;
+      y += dir;
+    }
+  } else {
+    register int x,y,z;
+
+    for (x = 0; x < QWX; x++)
+      for (y = 0; y < QWY; y++) {
+	z = 0;
+	if (x > 0) z += Qtem[x - 1][y];
+	if (x < (QWX - 1)) z += Qtem[x + 1][y];
+	if (y > 0) z += Qtem[x][y - 1];
+	if (y < (QWY - 1)) z += Qtem[x][y + 1];
+	TerrainMem[x][y] = (unsigned char)((z >>2) + Qtem[x][y]) >>1;
+      }
+  }
 }
 
 
@@ -528,16 +407,138 @@ void SmoothPSMap()
 }
 
 
-/* comefrom: PopDenScan */
-void DistIntMarket()
+/* comefrom: Simulate SpecialInit */
+void PTLScan()   	/* Does pollution, terrain, land value   */
 {
-  register int x, y, z;
+  int ptot, LVtot;
+  register int x, y, z, dis;
+  int Plevel, LVflag, loc, zx, zy, Mx, My, pnum, LVnum, pmax;
 
-  for (x = 0; x < SmX; x++)
-    for (y = 0; y < SmY; y++) {
-      z = GetDisCC(x <<2,y <<2);
-      z = z <<2;
-      z = 64 - z;
-      ComRate[x][y] = z;
+  for (x = 0; x < QWX; x++)
+    for (y = 0; y < QWY; y++)
+      Qtem[x][y] = 0;
+  LVtot = 0;
+  LVnum = 0;
+  for (x = 0; x < HWLDX; x++)
+    for (y = 0; y < HWLDY; y++) {
+      Plevel = 0;
+      LVflag = 0;
+      zx = x <<1;
+      zy = y <<1;
+      for (Mx = zx; Mx <= zx + 1; Mx++)
+	for (My = zy; My <= zy + 1; My++) {
+	  if (loc = (Map[Mx][My] & LOMASK)) {
+	    if (loc < RUBBLE) {
+	      Qtem[x >>1][y >>1] += 15;	/* inc terrainMem */
+	      continue;
+	    }
+	    Plevel += GetPValue(loc);
+	    if (loc >= ROADBASE)
+	      LVflag++;
+	  }
+	}
+/* XXX ???
+      if (Plevel < 0)
+	Plevel = 250;
+*/
+      if (Plevel > 255)
+	Plevel = 255;
+      tem[x][y] = Plevel;
+      if (LVflag) {			/* LandValue Equation */
+	dis = 34 - GetDisCC(x, y);
+	dis = dis <<2;
+	dis += (TerrainMem[x >>1][y >>1] );
+	dis -= (PollutionMem[x][y]);
+	if (CrimeMem[x][y] > 190) dis -= 20;
+	if (dis > 250) dis = 250;
+	if (dis < 1) dis = 1;
+	LandValueMem[x][y] = dis;
+	LVtot += dis;
+	LVnum++;
+      } else
+	LandValueMem[x][y] = 0;
+    }
+
+  if (LVnum)
+    LVAverage = LVtot / LVnum;
+  else
+    LVAverage = 0;
+
+  DoSmooth();
+  DoSmooth2();
+  pmax = 0;
+  pnum = 0;
+  ptot = 0;
+  for (x = 0; x < HWLDX; x++) {
+    for (y = 0; y < HWLDY; y++)  {
+      z = tem[x][y];
+      PollutionMem[x][y] = z;
+      if (z) {				/*  get pollute average  */
+	pnum++;
+	ptot += z;
+	/* find max pol for monster  */
+	if ((z > pmax) ||
+	    ((z == pmax) && (!(Rand16() & 3)))) {
+	  pmax = z;
+	  PolMaxX = x <<1;
+	  PolMaxY = y <<1;
+	}
+      }
+    }
   }
+  if (pnum)
+    PolluteAverage = ptot / pnum;
+  else
+    PolluteAverage = 0;
+
+  SmoothTerrain();
+
+  NewMapFlags[DYMAP] = NewMapFlags[PLMAP] = NewMapFlags[LVMAP] = 1;
+}
+
+
+/* comefrom: Simulate SpecialInit */
+void CrimeScan()
+{
+  int numz;
+  int totz;
+  register int x, y, z;
+  int cmax;
+
+  SmoothPSMap();
+  SmoothPSMap();
+  SmoothPSMap();
+  totz = 0;
+  numz = 0;
+  cmax = 0;
+  for (x = 0; x < HWLDX; x++)
+    for (y = 0; y < HWLDY; y++) {
+      if (z = LandValueMem[x][y]) {
+	++numz;
+	z = 128 - z;
+	z += PopDensity[x][y];
+	if (z > 300) z = 300;
+	z -= PoliceMap[x >>2][y >>2];
+	if (z > 250) z = 250;
+	if (z < 0) z = 0;
+	CrimeMem[x][y] = z;
+	totz += z;
+	if ((z > cmax) ||
+	    ((z == cmax) && (!(Rand16() & 3)))) {
+	  cmax = z;
+	  CrimeMaxX = x <<1;
+	  CrimeMaxY = y <<1;
+	}
+      } else {
+	CrimeMem[x][y] = 0;
+      }
+    }
+  if (numz)
+    CrimeAverage = totz / numz;
+  else
+    CrimeAverage = 0;
+  for (x = 0; x < SmX; x++)
+    for (y = 0; y < SmY; y++)
+      PoliceMapEffect[x][y] = PoliceMap[x][y];
+  NewMapFlags[DYMAP] = NewMapFlags[CRMAP] = NewMapFlags[POMAP] = 1;
 }
