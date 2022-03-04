611c61e68a3849d9129e2cea575d5048959f99f3
Leeor Dicker <leeor_net@yahoo.com>
2022-03-04 00:56:55 -0500

    Remove code and start deciphering graph drawing

--- src/w_graph.cpp
+++ src/w_graph.cpp
@@ -14,6 +14,7 @@
 
 #include "s_alloc.h"
 
+#include <SDL2/SDL.h>
 
 #include <algorithm>
 
@@ -55,43 +56,6 @@ unsigned char HistColor[] =
 };
 
 
-/*
-static void DisplaySimGraph(ClientData clientData)
-{
-  Graph *graph = (Graph *) clientData;
-  Tk_Window tkwin = graph->tkwin;
-  Pixmap pm = None;
-  Drawable d;
-
-  graph->flags &= ~VIEW_REDRAW_PENDING;
-
-//fprintf(stderr, "DisplaySimGraph token %d\n", graph->draw_graph_token);
-
-  assert(graph->draw_graph_token != 0);
-
-  if (graph->draw_graph_token != 0) {
-//    Tk_DeleteTimerHandler(graph->draw_graph_token);
-    graph->draw_graph_token = 0;
-  }
-
-  if (graph->visible && (tkwin != NULL) && Tk_IsMapped(tkwin)) {
-    DoUpdateGraph(graph);
-  }
-}
-*/
-
-
-/*
-void DestroySimGraph(ClientData clientData)
-{
-  Graph *graph = (Graph *) clientData;
-
-  DestroyGraph(graph);
-}
-*/
-
-
-
 void EventuallyRedrawGraph(Graph* graph)
 {
     /*
@@ -108,143 +72,6 @@ void EventuallyRedrawGraph(Graph* graph)
     }
     */
 }
-
-
-
-/*
-void SimGraphEventProc(ClientData clientData, XEvent *eventPtr)
-{
-  Graph *graph = (Graph *) clientData;
-
-  if ((eventPtr->type == Expose) && (eventPtr->xexpose.count == 0)) {
-    graph->visible = 1;
-    EventuallyRedrawGraph(graph);
-  } else if (eventPtr->type == MapNotify) {
-    graph->visible = 1;
-  } else if (eventPtr->type == UnmapNotify) {
-    graph->visible = 0;
-  } else if (eventPtr->type == VisibilityNotify) {
-    if (eventPtr->xvisibility.state == VisibilityFullyObscured)
-      graph->visible = 0;
-    else
-      graph->visible = 1;
-  } else if (eventPtr->type == ConfigureNotify) {
-    DoResizeGraph(graph,
-		  eventPtr->xconfigure.width,
-		  eventPtr->xconfigure.height);
-    EventuallyRedrawGraph(graph);
-  } else if (eventPtr->type == DestroyNotify) {
-    Tcl_DeleteCommand(graph->interp, Tk_PathName(graph->tkwin));
-    graph->tkwin = NULL;
-    if (graph->flags & VIEW_REDRAW_PENDING) {
-fprintf(stderr, "SimGraphEventProc Destroy token %d\n", graph->draw_graph_token);
-      assert(graph->draw_graph_token != 0);
-      if (graph->draw_graph_token != 0) {
-	Tk_DeleteTimerHandler(graph->draw_graph_token);
-	graph->draw_graph_token = 0;
-      }
-      graph->flags &= ~VIEW_REDRAW_PENDING;
-    }
-   Tk_EventuallyFree((ClientData) graph, DestroySimGraph);
-  }
-}
-*/
-
-/*
-int DoGraphCmd(ClientData clientData, Tcl_Interp *interp, int argc, char **argv)
-{
-  Graph *graph = (Graph *) clientData;
-  Tcl_HashEntry *ent;
-  int result = TCL_OK;
-  int (*cmd)();
-
-  if (argc < 2) {
-    return TCL_ERROR;
-  }
-
-  if (ent = Tcl_FindHashEntry(&GraphCmds, argv[1])) {
-    cmd = (int (*)())ent->clientData;
-    Tk_Preserve((ClientData) graph);
-    result = cmd(graph, interp, argc, argv);
-    Tk_Release((ClientData) graph);
-  } else {
-    Tcl_AppendResult(interp, "unknown command name: \"",
-		     argv[0], " ", argv[1], "\".", (char *) NULL);
-    result = TCL_ERROR;
-  }
-  return result;
-}
-*/
-
-/*
-int GraphViewCmd(ClientData clientData, Tcl_Interp* interp, int argc, char** argv)
-{
-    Graph* graph;
-    Tk_Window tkwin = (Tk_Window)clientData;
-
-    if (argc < 2) {
-        Tcl_AppendResult(interp, "wrong # args:  should be \"",
-            argv[0], " pathName ?options?\"", (char*)NULL);
-        return TCL_ERROR;
-    }
-
-    tkwin = Tk_CreateWindowFromPath(interp, tkwin,
-        argv[1], (char*)NULL);
-    if (tkwin == NULL) {
-        return TCL_ERROR;
-    }
-
-    graph = (Graph*)malloc(sizeof(Graph));
-
-    graph->tkwin = tkwin;
-    graph->interp = interp;
-    graph->flags = 0;
-
-    Tk_SetClass(graph->tkwin, "GraphView");
-    Tk_CreateEventHandler(graph->tkwin,
-        VisibilityChangeMask |
-        ExposureMask |
-        StructureNotifyMask,
-        SimGraphEventProc, (ClientData)graph);
-    Tcl_CreateCommand(interp, Tk_PathName(graph->tkwin),
-        DoGraphCmd, (ClientData)graph, (void (*)()) NULL);
-
-    Tk_MakeWindowExist(graph->tkwin);
-
-    if (getenv("XSYNCHRONIZE") != NULL)
-    {
-        XSynchronize(Tk_Display(tkwin), 1);
-    }
-
-    InitNewGraph(graph);
-    DoNewGraph(graph);
-
-    if (ConfigureSimGraph(interp, graph, argc - 2, argv + 2, 0) != TCL_OK)
-    {
-        //* XXX: destroy graph
-        Tk_DestroyWindow(graph->tkwin);
-        return TCL_ERROR;
-    }
-
-    interp->result = Tk_PathName(graph->tkwin);
-    return TCL_OK;
-}
-*/
-
-/*
-int ConfigureSimGraph(Tcl_Interp* interp, Graph* graph, int argc, char** argv, int flags)
-{
-    if (Tk_ConfigureWidget(interp, graph->tkwin, GraphConfigSpecs, argc, argv, (char*)graph, flags) != TCL_OK)
-    {
-        return TCL_ERROR;
-    }
-
-    Tk_SetBackgroundFromBorder(graph->tkwin, graph->border);
-
-    EventuallyRedrawGraph(graph);
-    return TCL_OK;
-}
-*/
 
 
 void drawMonth(const std::array<int, HistoryLength>& history, unsigned char* s, float scale)
@@ -467,31 +294,21 @@ void InitNewGraph(Graph* graph)
 }
 
 
-#define BORDER 5
+constexpr auto BORDER = 5;
+
 
 void DoUpdateGraph(Graph* graph)
 {
-    /*
-    Display* dpy;
-    GC gc;
-    Pixmap pm;
-    int* pix;
     unsigned char** hist;
-    int w, h, mask, i, j, x, y;
-    XPoint points[121];
+    int mask, j, x, y;
+    SDL_Point points[121]{};
     int year = (CityTime / 48) + StartingYear;
     int month = (CityTime / 4) % 12;
     int do_top_labels = 0;
     int do_right_labels = 0;
     int top_label_height = 30;
     int right_label_width = 65;
-    int tx, ty;
     float sx, sy;
-
-    if (!graph->visible)
-    {
-        return;
-    }
 
     if (graph->range == 10)
     {
@@ -502,29 +319,16 @@ void DoUpdateGraph(Graph* graph)
         hist = History120;
     }
 
-    dpy = graph->x->dpy;
-    gc = graph->x->gc;
-    pm = graph->pixmap;
-    pix = graph->pixels;
+    int w = graph->w_width;
+    int h = graph->w_height;
 
-    w = graph->w_width;
-    h = graph->w_height;
+    //XSetFont(graph->x->dpy, graph->x->gc, graph->fontPtr->fid);
+    //XSetLineAttributes(dpy, gc, 3, LineSolid, CapButt, JoinBevel);
+    //XSetForeground(dpy, gc, pix[COLOR_LIGHTGRAY]);
+    //XFillRectangle(dpy, pm, gc, 0, 0, w, h);
 
-    XSetFont(graph->x->dpy, graph->x->gc, graph->fontPtr->fid);
-    XSetLineAttributes(dpy, gc, 3, LineSolid, CapButt, JoinBevel);
-
-    if (graph->x->color)
-    {
-        XSetForeground(dpy, gc, pix[COLOR_LIGHTGRAY]);
-    }
-    else
-    {
-        XSetForeground(dpy, gc, pix[COLOR_WHITE]);
-    }
-
-    XFillRectangle(dpy, pm, gc, 0, 0, w, h);
-
-    tx = BORDER; ty = BORDER;
+    int tx = BORDER;
+    int ty = BORDER;
 
     if ((w -= (2 * BORDER)) < 1) w = 1;
     if ((h -= (2 * BORDER)) < 1) h = 1;
@@ -545,13 +349,12 @@ void DoUpdateGraph(Graph* graph)
     sx = ((float)w) / 120.0; sy = ((float)h) / 256.0;
 
     mask = graph->mask;
-    for (i = 0; i < HISTORIES; i++, mask >>= 1, hist++)
+    for (int i = 0; i < HISTORIES; i++, mask >>= 1, hist++)
     {
         if (mask & 1)
         {
             int fg = COLOR_WHITE;
             int bg = COLOR_BLACK;
-            Pixmap stipple = None;
 
             for (j = 0; j < 120; j++)
             {
@@ -559,88 +362,32 @@ void DoUpdateGraph(Graph* graph)
                 y = ty + ((int)(h - (((float)(*hist)[j]) * sy)));
                 points[j].x = x; points[j].y = y;
             }
+
             x = tx + (j * sx);
             points[j].x = x; points[j].y = y;
 
-            if (graph->x->color)
-            {
-                XSetForeground(dpy, gc, pix[HistColor[i]]);
-            }
-            else
-            {
-                switch (i)
-                {
-                case 0: // res
-                    stipple = graph->x->gray50_stipple;
-                    break;
-
-                case 1: // com
-                    stipple = graph->x->gray25_stipple;
-                    break;
-
-                case 2: // ind
-                    stipple = graph->x->gray75_stipple;
-                    break;
-
-                case 3: // cash
-                    fg = COLOR_BLACK;
-                    break;
-
-                case 4: // crime
-                    stipple = graph->x->horiz_stipple;
-                    break;
-
-                case 5: // pol
-                    stipple = graph->x->vert_stipple;
-                    break;
-                }
-
-                if (stipple != None)
-                {
-                    XSetStipple(graph->x->dpy, gc, stipple);
-                    XSetTSOrigin(graph->x->dpy, gc, 0, 0);
-                    XSetForeground(graph->x->dpy, gc, pix[fg]);
-                    XSetBackground(graph->x->dpy, gc, pix[bg]);
-                    XSetFillStyle(graph->x->dpy, gc, FillOpaqueStippled);
-                }
-                else
-                {
-                    XSetForeground(graph->x->dpy, gc, pix[fg]);
-                }
-            }
-
-            XDrawLines(dpy, pm, gc, points, 121, CoordModeOrigin);
-
-            if (!graph->x->color && (stipple != None))
-            {
-                XSetFillStyle(graph->x->dpy, gc, FillSolid);
-            }
+            //XSetForeground(dpy, gc, pix[HistColor[i]]);
+            //XDrawLines(dpy, pm, gc, points, 121, CoordModeOrigin);
+            //XSetFillStyle(graph->x->dpy, gc, FillSolid);
 
             if (do_right_labels)
             {
-                if (graph->x->color)
-                {
-                    XSetForeground(dpy, gc, pix[HistColor[i]]);
-                    XDrawString(graph->x->dpy, pm, graph->x->gc, x + 4, y + 5, HistName[i], strlen(HistName[i]));
-                    XDrawString(graph->x->dpy, pm, graph->x->gc, x + 5, y + 4, HistName[i], strlen(HistName[i]));
+                // This fakes a 'glow' effect around the text which is rendered below in black
+                //XSetForeground(dpy, gc, pix[HistColor[i]]);
+                //XDrawString(graph->x->dpy, pm, graph->x->gc, x + 4, y + 5, HistName[i], strlen(HistName[i]));
+                //XDrawString(graph->x->dpy, pm, graph->x->gc, x + 5, y + 4, HistName[i], strlen(HistName[i]));
 
-                    XSetForeground(dpy, gc, pix[COLOR_BLACK]);
-                    XDrawString(graph->x->dpy, pm, graph->x->gc, x + 5, y + 5, HistName[i], strlen(HistName[i]));
-                }
-                else
-                {
-                    XSetForeground(dpy, gc, pix[COLOR_BLACK]);
-                    XDrawString(graph->x->dpy, pm, graph->x->gc, x + 5, y + 5, HistName[i], strlen(HistName[i]));
-                }
+                //XSetForeground(dpy, gc, pix[COLOR_BLACK]);
+                //XDrawString(graph->x->dpy, pm, graph->x->gc, x + 5, y + 5, HistName[i], strlen(HistName[i]));
             }
         }
     }
 
-    XSetLineAttributes(dpy, gc, 1, LineSolid, CapButt, JoinMiter);
+    //XSetLineAttributes(dpy, gc, 1, LineSolid, CapButt, JoinMiter);
 
-    XSetForeground(dpy, gc, pix[COLOR_BLACK]);
-    XDrawLine(dpy, pm, gc, tx, ty - 1, tx + w, ty - 1);
-    XDrawLine(dpy, pm, gc, tx, ty + h, tx + w, ty + h);
+    //XSetForeground(dpy, gc, pix[COLOR_BLACK]);
+    //XDrawLine(dpy, pm, gc, tx, ty - 1, tx + w, ty - 1);
+    //XDrawLine(dpy, pm, gc, tx, ty + h, tx + w, ty + h);
 
     if (graph->range == 10)
     {
@@ -648,16 +395,18 @@ void DoUpdateGraph(Graph* graph)
         {
             int xx, yy;
             xx = tx + (x * sx);
-            XDrawLine(dpy, pm, gc, xx, ty - 1, xx, ty + h);
+
+            //XDrawLine(dpy, pm, gc, xx, ty - 1, xx, ty + h);
 
             if (do_top_labels)
             {
-                char buf[256];
-
-                sprintf(buf, "%d", year--);
+                //char buf[256];
+                //sprintf(buf, "%d", year--);
+                
                 xx = tx + (x * sx) + 2;
                 yy = ty - ((year & 1) ? 4 : 20);
-                XDrawString(graph->x->dpy, pm, graph->x->gc, xx, yy, buf, strlen(buf));
+                
+                //XDrawString(graph->x->dpy, pm, graph->x->gc, xx, yy, buf, strlen(buf));
             }
         }
     }
@@ -673,20 +422,18 @@ void DoUpdateGraph(Graph* graph)
         {
             int xx, yy;
             xx = tx + (x * sx);
-            XDrawLine(dpy, pm, gc, xx, ty - 1, xx, ty + h);
+            //XDrawLine(dpy, pm, gc, xx, ty - 1, xx, ty + h);
             if (do_top_labels)
             {
-                char buf[256];
-
-                sprintf(buf, "%d0", year--);
+                //char buf[256];
+                //sprintf(buf, "%d0", year--);
 
                 xx = tx + (x * sx) + 2;
                 yy = ty - ((year & 1) ? 4 : 20);
-                XDrawString(graph->x->dpy, pm, graph->x->gc, xx, yy, buf, strlen(buf));
+                //XDrawString(graph->x->dpy, pm, graph->x->gc, xx, yy, buf, strlen(buf));
             }
         }
     }
 
-    XCopyArea(graph->x->dpy, graph->pixmap, Tk_WindowId(graph->tkwin), graph->x->gc, 0, 0, graph->w_width, graph->w_height, 0, 0);
-    */
+    //XCopyArea(graph->x->dpy, graph->pixmap, Tk_WindowId(graph->tkwin), graph->x->gc, 0, 0, graph->w_width, graph->w_height, 0, 0);
 }
