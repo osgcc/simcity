771d8736fb8bf2b1dd70961c3c48e8cf20ccb81f
Leeor Dicker <leeor_net@yahoo.com>
2022-03-06 00:11:53 -0500

    Add color defs

--- src/g_map.h
+++ src/g_map.h
@@ -19,9 +19,11 @@ namespace Colors
 	constexpr SDL_Color DarkBlue{ 0, 0, 139, 255 };
 	constexpr SDL_Color DarkGreen{ 0, 100, 0, 255 };
 	constexpr SDL_Color Gold{ 255, 215, 0, 255 };
+	constexpr SDL_Color Green{ 0, 128, 0, 255 };
 	constexpr SDL_Color LightBlue{ 0, 191, 255, 255 };
 	constexpr SDL_Color LightGreen{ 50, 205, 50, 255 };
 	constexpr SDL_Color LightGrey{ 211, 211, 211, 255 };
+	constexpr SDL_Color MediumBlue{ 0, 0, 205, 255 };
 	constexpr SDL_Color Olive{ 107, 142, 35 };
 	constexpr SDL_Color Orange{ 255, 165, 0, 255 };
 	constexpr SDL_Color Red{ 255, 0, 0, 255 };
