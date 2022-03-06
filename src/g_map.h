0b8c521b19196d8ef5ae7b1b551c5fecc10d50fb
Leeor Dicker <leeor_net@yahoo.com>
2022-03-05 21:28:07 -0500

    Add color constants

--- src/g_map.h
+++ src/g_map.h
@@ -16,12 +16,15 @@
 namespace Colors
 {
 	constexpr SDL_Color LightGrey{ 211, 211, 211, 255 };
+	constexpr SDL_Color Gold{ 255, 215, 0, 255 };
 	constexpr SDL_Color Yellow{ 255, 255, 0, 255 };
 	constexpr SDL_Color Orange{ 255, 165, 0, 255 };
+	constexpr SDL_Color Olive{ 107, 142, 35 };
 	constexpr SDL_Color Red{ 255, 0, 0, 255 };
 	constexpr SDL_Color DarkGreen{ 0, 100, 0, 255 };
 	constexpr SDL_Color LightGreen{ 50, 205, 50, 255 };
 	constexpr SDL_Color LightBlue{ 0, 191, 255, 255 };
+	constexpr SDL_Color DarkBlue{ 0, 0, 139, 255 };
 	constexpr SDL_Color Clear{ 0, 0, 0, 0 };
 };
 
