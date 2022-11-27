0ffdeb062928cadd9f3c1142f9c04bcfe3acc0d2
Leeor Dicker <leeor_net@yahoo.com>
2022-11-26 20:28:09 -0500

    Add base window class

---
+++ src/WindowBase.h
@@ -0,0 +1,24 @@
+#pragma once
+
+
+class WindowBase
+{
+public:
+	WindowBase() = default;
+	WindowBase(const WindowBase&) = delete;
+	const WindowBase& operator=(const WindowBase&) = delete;
+
+	virtual ~WindowBase() = default;
+
+	bool visible() const { return mVisible; }
+	
+	void show() { mVisible = true; }
+	void hide() { mVisible = false; }
+	
+	virtual void draw() = 0;
+	virtual void update() = 0;
+
+private:
+
+	bool mVisible{ true };
+};
