4da89cf8157ab59e5711e252ec8a7e81c73ed023
Leeor Dicker <leeor_net@yahoo.com>
2022-12-12 14:48:32 -0500

    Removed unused #define's

--- src/s_scan.h
+++ src/s_scan.h
@@ -14,29 +14,6 @@
 
 #include "Point.h"
 
-#define ALMAP 0 /* all */
-#define REMAP 1 /* residential */
-#define COMAP 2 /* commercial */
-#define INMAP 3 /* industrial */
-
-#define PRMAP 4 /* power */
-#define RDMAP 5 /* road */
-
-#define PDMAP 6 /* population density */
-#define RGMAP 7 /* rate of growth */
-
-#define TDMAP 8 /* traffic density */
-#define PLMAP 9 /* pollution */
-#define CRMAP 10 /* crime */
-#define LVMAP 11 /* land value */
-
-#define FIMAP 12 /* fire radius */
-#define POMAP 13 /* police radius */
-#define DYMAP 14 /* dynamic */
-
-#define NMAPS 15
-
-
 const Point<int>& pollutionMax();
 void powerScan();
 void pollutionAndLandValueScan();
